625c67538a2c4fdec51599768fa2bdb22ce6b8c7
mihailponomarev <maikalpanda2009@yandex.ru>
2023-01-18 11:23:56 +0300

    test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,27 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Write number");
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+
+        if (number % 2 != 0) {
+            System.out.println("Число нечетное");
+        } else {
+            System.out.println("Число четное");
+        }
+
+
+        if (number % 10 == 3) {
+            System.out.println("Число окончивается на 3");
+        } else {
+            System.out.println("Число не окончивается на 3");
+        }
+
+
         System.out.println("Hello world!");
     }
 }
